f12876246f382e57b1fe278c39933663210b7962
EstefanoC <estefano.jesus6@gmail.com>
2021-05-21 19:05:35 -0400

    mend

--- src/components/helpers/map.js
+++ src/components/helpers/map.js
@@ -7,7 +7,7 @@ export const Map = () => {
             <div className="row">
 
                 <div className="col-12 col-md-6 h-100 w-100">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d5187.172002326249!2d-102.8738736714653!3d23.174415288046184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sve!4v1621557600860!5m2!1ses-419!2sve" style={{border: "0", borderRadius: '2rem', height: "100%", width: "100%"}} loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d5187.172002326249!2d-102.8738736714653!3d23.174415288046184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sve!4v1621557600860!5m2!1ses-419!2sve" style={{border: "0", borderRadius: '2rem', height: "100%", width: "100%"}} loading="lazy" title={"google maps"}></iframe>
                 </div>
 
                 <div className="col-12 col-md-6 text-map">
